bfb34e67567cf4ed1246fb3a04fe22d4ea20e670
Kauã <139062285+KauaAraujoSilva@users.noreply.github.com>
2026-01-18 10:07:51 -0300

    usando strings

---
+++ Usando_string.py
@@ -0,0 +1,50 @@
+"""
+Formatação básica de strings
+s - string
+d - int
+f - float
+.<número de dígitos>f
+x ou X - Hexadecimal
+(Caractere)(><^)(quantidade)
+> - Esquerda
+< - Direita
+^ - Centro
+= - Força o número a aparecer antes dos zeros
+Sinal - + ou -
+Ex.: 0>-100,.1f
+Conversion flags - !r !s !a 
+"""
+'''variavel = 'ABC'
+print(f'{variavel}')
+print(f'{variavel: >10}')
+print(f'{variavel: <10}.')
+print(f'{variavel: ^10}.')
+print(f'{1000.4873648123746:0=+10,.1f}')
+print(f'O hexadecimal de 1500 é {1500:08X}')
+print(f'{variavel!r}')'''
+
+frase = ('Ola mundo! \nola, pitagoras.')
+i = 0
+qtd_apareceu_mais_vezes = 0
+letra_apareceu_mais_vezes = ''
+
+while i < len(frase):
+    letra_atual = frase[i]
+
+    if letra_atual == ' ':
+        i += 1
+        continue
+
+    qtd_atual = frase.count(letra_atual)
+
+    if qtd_apareceu_mais_vezes <= qtd_atual:
+        qtd_apareceu_mais_vezes = qtd_atual
+        letra_apareceu_mais_vezes = letra_atual
+
+    i += 1
+
+print(
+    'A letra que apareceu mais vezes foi '
+    f'"{letra_apareceu_mais_vezes}" que apareceu '
+    f'{qtd_apareceu_mais_vezes}x'
+)
