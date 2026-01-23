fee8dd62971c4f62d5914cd1ad93f979c095f8d3
Kauã <139062285+KauaAraujoSilva@users.noreply.github.com>
2026-01-23 17:05:02 -0300

    Lista de Compras

--- ListaDeCompras.py
+++ ListaDeCompras.py
@@ -5,23 +5,36 @@ inserir, apagar e listar valores da sua lista
 Não permita que o programa quebre com 
 erros de índices inexistentes na lista.
 """
+import os
+
 lista = []
 while True:
-    Opções = input('Selecione uma opção \n' \
-            '[i]nserir  [a]pagar  [l]istar: ').lower()
-    if Opções == 'a':
-        lista.clear()
-    try:
-        if Opções == 'l':
-            print(lista)
-            continue
-        elif Opções == 'i':
-            Opção_I = input('Digite o que dejesa inserir: ')
-            lista.append(Opção_I)
-            lista.count()
-        elif lista is False:
-            print('ista falsa')
-    except ValueError:
-        print('dnfiadjf')
-    except TypeError:
-        print('DJFHADIF')
+    print('Selecione uma opção')
+    opcao = input('[i]nserir [a]pagar [l]istar: ')
+
+    if opcao == 'i':
+        os.system('clear')
+        valor = input('Valor: ')
+        lista.append(valor)
+    elif opcao == 'a':
+        indice_str = input('Escolha o índice para apagar: ')
+
+        try:
+            indice = int(indice_str)
+            del lista[indice]
+        except ValueError:
+            print('Por favor digite número int.')
+        except IndexError:
+            print('Índice não existe na lista')
+        except Exception:
+            print('Erro desconhecido')
+    elif opcao == 'l':
+        os.system('clear')
+
+        if len(lista) == 0:
+            print('Nada para listar')
+
+        for i, valor in enumerate(lista):
+            print(i, valor)
+    else:
+        print('Por favor, escolha i, a ou l.')
